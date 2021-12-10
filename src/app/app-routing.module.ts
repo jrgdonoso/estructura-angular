3b976e1b5e32440ea2a812db8069bb92140052a9
jorgeDonoso <jrg.donoso@gmail.com>
2021-12-10 17:38:49 -0300

    Se establece base modular del proyecto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { NotFoundComponent } from './core/shared/components/not-found.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', loadChildren:()=>import('./public/public.module').then(m=>m.PublicModels)},
+  {path: '**', component: NotFoundComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: false})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/public/public-routing.module.ts
@@ -0,0 +1,26 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { PublicComponent } from './public.component';
+import { HomeComponent } from './home/containers/home.component';
+import { LoginComponent } from './login/containers/login.component';
+
+
+const rutas: Routes=[
+    {
+       path: '',component:PublicComponent, children:
+       [
+           { path:'', redirectTo:'home',pathMatch:'full' },
+           {path:'home',component:HomeComponent},
+           {path:'login', component:LoginComponent}
+       ] 
+    }
+];
+
+@NgModule({
+    imports: [RouterModule.forChild(rutas)],
+    exports: [RouterModule]
+})
+
+export class PublicRoutingModule{
+}
